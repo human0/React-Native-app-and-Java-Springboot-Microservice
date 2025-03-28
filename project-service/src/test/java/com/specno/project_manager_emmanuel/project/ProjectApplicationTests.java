ff256bc61d78b1a25d7d299f60c59fa531a33e1e
Emmanuel <emmanuelhuman01@gmail.com>
2025-03-28 21:23:05 +0200

    feature[sp-7]: Initilize project (and task) service

---
+++ project-service/src/test/java/com/specno/project_manager_emmanuel/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specno.project_manager_emmanuel.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
